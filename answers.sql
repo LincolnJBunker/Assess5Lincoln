8ff4f8560495582d7b8e7dc04058ab8b7f2c656b
Lincoln Bunker <lincolnbunker03@gmail.com>
2024-02-09 11:28:42 -0700

    problems 5 and 6

--- answers.sql
+++ answers.sql
@@ -11,3 +11,8 @@ SELECT SUM(num_cupcakes) FROM orders WHERE processed = 'f';
 SELECT cupcakes.name, SUM(orders.num_cupcakes) FROM cupcakes LEFT JOIN orders ON cupcakes.id = orders.cupcake_id GROUP BY cupcakes.name ORDER BY cupcakes.name;
 
 //problem 5
+SELECT customers.email, SUM(orders.num_cupcakes) FROM customers LEFT JOIN orders ON customers.id = orders.customer_id GROUP BY customers.email ORDER BY customers.email;
+
+//problems 6
+SELECT MIN(customers.fname), MIN(customers.lname), MIN(customers.email) from customers INNER JOIN orders ON customers.id = orders.customer_id INNER JOIN cupcakes ON orders.cupcake_id = cupcakes.id WHERE cupcakes.name = 'funfetti' AND orders.processed = 't';
+
